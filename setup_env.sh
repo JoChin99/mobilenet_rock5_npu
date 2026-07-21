42f24a7fdf9588122ed4231b8f9c01ad35550e05
JoChin99 <juiningc99@hotmail.com>
2026-07-21 18:39:32 +0100

    Added shell script and requirement.txt for python packages

---
+++ setup_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+ROCK5B_DIR="rock5b_env"
+
+if [ ! -d "$ROCK5B_DIR" ]; then
+    echo "-I- Creating virtual environment in ./$ROCK5B_DIR"
+    python3 -m venv "$ROCK5B_DIR"
+else
+    echo "-I- $ROCK5B_DIR environment already exists."
+fi
+
+source $ROCK5B_DIR/bin/activate
+pip install --upgrade pip
+pip install -r requirement.txt
+
+echo "-I- Done"
+echo "-I- The environment can be activated with this command: source $ROCK5B_DIR/bin/activate"
